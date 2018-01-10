0623179171bc5bc25533592f360bd5fdf4bcd0bd
you Name <you@example.com>
2018-01-11 04:44:03 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.template')
+@section('content')
+
+<<div class="col-md-10 col-md-offset-2">
+  <div class="panel panel-primary">
+    <div class="panel-heading"> Home
+    <div class="panel-title pull-right">
+      </div></div>
+      <div class="panel-body">
+     <div class="table-responsive">
+     <table class="table">
+@endsection
